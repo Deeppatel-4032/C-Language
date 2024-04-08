ab2af93de6fa32a7004ba62f7ffffc3570b9edc0
Deeppatel-4032 <deeppatel7868@gmail.com>
2024-04-08 19:33:16 +0530

    c programe

--- mark_else_if.c
+++ mark_else_if.c
@@ -12,19 +12,19 @@ int main() {
     }
     else if(mark >= 33 && mark < 50) { 
 
-        printf("you are C gread");
+        printf("you are C grade");
     }
     else if(mark >= 50 && mark < 70) {
 
-        printf("you are B gread");
+        printf("you are B grade");
     }
     else if(mark >= 70 && mark < 80) {
 
-        printf("you are A gread");
+        printf("you are A grade");
     }
     else if(mark > 80 &&  mark < 100) {
 
-        printf("you are A+ gread");
+        printf("you are A+ grade");
     }
 
     return 0;
